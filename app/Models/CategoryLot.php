9ee68a098d7c840974de38b01413ac9bbd754dc4
Vitalii Berezhnyi <vitalijber2004@gmail.com>
2024-04-13 00:07:09 +0300

    Add models

---
+++ app/Models/CategoryLot.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CategoryLot extends Model
+{
+    use HasFactory;
+    protected $table = 'categories_lots';
+}
