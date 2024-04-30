62442c972102cc9a506f521cdf4c3d0c76d9b513
Vitalii Berezhnyi <vitalijber2004@gmail.com>
2024-04-30 22:29:36 +0300

    Add grouped routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\BidsController;
+use App\Http\Controllers\LotsManagementController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserInfoController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -23,5 +26,48 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/catalog', function (){
+
+});
+
+Route::get('/lots/{id}', function (int $id){
+
+});
+
+Route::get('/{category_id}', function (int $category_id){
+
+});
+
+Route::get('/menu', function (){
+
+});
+
+Route::get('/tracked-lots', function (){
+
+});
+
+Route::middleware('auth')->controller(LotsManagementController::class)->group(function () {
+    Route::get('/user-lots', 'index');
+    Route::get('/user-lots/{id}', 'show');
+    Route::get('/user-lots/{id}/edit', 'edit');
+    Route::patch('/user-lots/{id}', 'update');
+    Route::delete('/user-lots/{id}', 'destroy');
+    Route::get('/user-lots/create', 'create');
+    Route::post('/user-lots', 'store');
+});
+
+Route::middleware('auth')->controller(BidsController::class)->group(function () {
+    Route::get('/bids', 'index');
+    Route::get('/bids/{id}', 'show');
+    Route::delete('/bids/{id}', 'remove'); 
+});
+
+Route::middleware('auth')->controller(UserInfoController::class)->group(function () {
+    Route::get('/userinfo', 'index');
+    Route::get('/userinfo/edit', 'edit');
+    Route::patch('/userinfo', 'update'); 
+});
+
 
 require __DIR__.'/auth.php';
